9d56686a72182f75fb6760ee542e529620f12944
Arieh Smith <arieh.smith@gmail.com>
2016-08-13 14:29:52 -0400

    Dictionary labels are characters

--- script.py
+++ script.py
@@ -17,22 +17,28 @@ for filename in glob.glob("gnts/*.gnt"):
 			break
 		print(packed_length)
 		length = struct.unpack("<I", packed_length)[0]
-		label = struct.unpack(">H", f.read(2))[0]
-		label -= 0xb0a1 # CASIA labels start at 0xb0a1
+		tag_name = f.read(2)
+		# label = struct.unpack(">H", tag_name)[0]
+		# label -= 0xb0a1 # CASIA labels start at 0xb0a1
+		tag_name = tag_name.decode("gb2312")
 		width = struct.unpack("<H", f.read(2))[0]
 		height = struct.unpack("<H", f.read(2))[0]
 		raw_bytes = f.read(height*width)
 		bytes = struct.unpack("{}B".format(height*width), raw_bytes)
 		existing_labels = full_data.keys()
-		if (label in existing_labels) or (len(existing_labels) < num_classes):
+		if (tag_name in existing_labels) or (len(existing_labels) < num_classes):
 			image = numpy.array(bytes).reshape(height, width)
 			image = scipy.misc.imresize(image, (side, side))
-			if (1==0):
-				scipy.misc.imsave("%d.bmp" % (counter), image)
+			# scipy.misc.imsave("%s.bmp" % (tag_name), image)
 			image = (image.astype(float) / 256) - 0.5 # normalize to [-0.5,0.5] to avoid saturation
-			full_data[label].append(image)
+			full_data[tag_name].append(image)
 			counter = counter + 1
 	f.close()
+
+# structure of full_data -- dictionary with 60 keys,
+# each of which is a character pointing to 60 examples
+# each example is a 224x224 matrix
+# Ignore the below
 
 # dic = {}
 # for line in open("gbdb").read().splitlines():
